ea000fa4c8a125bdaa8c49d4568993b6b17151fb
Slylnss <s.alonsious@web-siouss.be>
2017-10-09 20:19:01 +0200

    Avancée exo 4

---
+++ ex4.js
@@ -0,0 +1,61 @@
+//Exercice 1
+
+var nbr=0;
+
+while (nbr<=20){
+	
+	if (nbr%2){
+		console.log(nbr + " est impair");
+	}
+	else{
+		console.log(nbr + " est pair");
+	}
+	nbr +=1;
+}
+
+//Exercice 2
+
+var nbr=9;
+var result= 0;
+
+for (i=0; i<=10; i++)
+{
+	result = nbr * i;
+	console.log(i + " * " + nbr + " = " + result);
+}
+
+/*Exercice 3
+
+var score=60;
+
+
+	var grade = function(score){
+		score+=1;
+		return score;				
+	}
+
+while (grade(score) <100){
+
+		if(score >90){
+			var message= "votre grade est A";
+		}else if (score >80 && score <89){
+			var message= "Votre grade est B";
+		}else if (score >70 && score <79){
+			var message= "Votre grade est C";
+		}else if (score >65 && score <69){
+			var message= "Votre grade est D";
+		}else {
+			var message= "Votre grade est F";
+		}
+
+
+
+	console.log(message);
+}*/
+
+//Exercice 4
+
+symbole = 5;
+
+for (i=0; i<symbole; i++){
+}
