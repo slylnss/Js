d1ed9ffbe63c2596e72e6b063dcc5a856a049c21
Slylnss <s.alonsious@web-siouss.be>
2017-10-17 13:51:17 +0200

    mise a jour exo8

--- ex8.js
+++ ex8.js
@@ -8,13 +8,65 @@ let Character = {
 don = function(){
 		console.log("Vous recevez !");
 		for(items_to_give in Character){
-		Character.items_to_give[Math.floor(Math.random()*3)];
+			var item = Character.items_to_give[Math.floor(Math.random()*3)];
 			};
-		return Character.items_to_give;	
+		return console.log(item);	
 	};
 
 
 don();
+
+//Création Shop
+
+var Armes = {
+	description: function(arme, nom_arme, dégats_physique, dégats_magique, minLevel, available){
+	this.type = arme;
+	this.nom_arme = nom_arme;
+	this.dégats_physique = dégats_physique;
+	this.dégats_magique = dégats_magique;
+	this.minLevel = minLevel;
+	this.available = available;
+	},
+
+	renvoie: function (){
+		var caractéristiques = 
+		"Type :" + this.type + 
+		"Nom : " + this.nom_arme +
+		"Dégats Physique : " +" "+ this.dégats_physique + " "+
+		"Dégats Magique : " +" "+ this.dégats_magique + " "+
+		"Level Minimum : " +" "+ this.minLevel + " "+
+		"Disponible : " +" "+ this.available;
+		return caractéristiques;
+	}
+};
+
+var Epée = Object.create(Armes);
+Epée.description('arme', 'Epée', 5, 1, 1, true);
+var Baton = Object.create(Armes);
+Baton.description('arme', 'Baton', 3, 2, 5, false);
+var Dague = Object.create(Armes);
+Dague.description('arme', 'Dague', 2, 3, 10, true);
+
+var armes = [];
+armes.push(Epée);
+armes.push(Baton);
+armes.push(Dague);
+
+function affiche() {
+armes.forEach(function(Armes){
+	console.log(Armes.renvoie());
+});
+}
+
+affiche();
+
+function armes_dispo() {
+	if(function renvoie(available.caractéristiques == true){
+		console.log(Armes.renvoie());
+	}	
+}
+
+armes_dispo();
 
 
 
