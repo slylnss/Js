aae357e9477b9076e49cb70e4884a09f032269c7
Slylnss <s.alonsious@web-siouss.be>
2017-10-19 14:27:33 +0200

    finition exo9

--- ex9.js
+++ ex9.js
@@ -2,7 +2,7 @@ let message = " ";
 let essai = 0;
 let min = Math.floor(Math.random(0,20)*20);
 let max = Math.floor(Math.random(0,80)*80);
-let numChoisi = prompt("Entrez un numéro entre 0 et 80");
+
 
 
 let numAleatoire = function(min, max) {
@@ -10,39 +10,36 @@ let numAleatoire = function(min, max) {
 };
 
 let num = numAleatoire(min,max);
-
+console.log("La bonne réponse est " + num);
 jeu = function() {
-
-if(numChoisi>0 && numChoisi<80){
-
-	while (numChoisi != num){
-
-		if (numChoisi < num){
-			console.log(numChoisi);
-			message = console.log("C'est plus !");
-			numChoisi = prompt("Entrez un numéro entre 0 et 80");
-
+let numChoisi = prompt("Entrez un numéro entre 0 et 80");	
+	if(numChoisi>0 && numChoisi<80){
+		while (numChoisi != num){
+			if (numChoisi < num){
+				console.log(numChoisi);
+				message = console.log("C'est plus !");
+				essai +=1;
+			}
+			else if (numChoisi > num) {
+				console.log(numChoisi);
+				message = console.log("C'est moins!");
+				essai +=1;
+			}
+		numChoisi = prompt("Entrez un numéro entre 0 et 80");
 		}
-		else if (numChoisi > num) {
-			console.log(numChoisi);
-			message = console.log("C'est moins!");
+		if (numChoisi==num){
+				alert("Tu as gagné!");
+			}
+		else{
 			numChoisi = prompt("Entrez un numéro entre 0 et 80");
 		}
-
+		
 	}
-}
-
-else if (numChoisi == num) {
-			console.log(numChoisi);
-			message = console.log("Tu as gagné !");
-		}
-
-else{
-	console.log(numChoisi);
-	message = alert("Ton numéro ne respecte pas les conditions");
-}
-
-return message;
+	else{
+		console.log(numChoisi);
+		message = alert("Ton numéro ne respecte pas les conditions");
+	}
+	return message;
 }
 
 jeu();
