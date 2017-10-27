092726d21304337d3075a7d30696b41e0a7aa1d0
Slylnss <s.alonsious@web-siouss.be>
2017-10-27 12:15:56 +0200

    avancée ex011, reste les finitions

--- ex11.js
+++ ex11.js
@@ -1,4 +1,4 @@
-var resultats = document.getElementById("#info");
+var resultats = document.getElementById("info");
 
 function ajax_get_json(){
     var xmlhttp;
@@ -11,11 +11,44 @@ function ajax_get_json(){
     }
 
     xmlhttp.onreadystatechange = function (){
-        if(xmlhttp.readystate === 4 && xmlhttp.status === 200){
-            alert(xmlhttp.responseText);
+        if(xmlhttp.readyState === 4 && xmlhttp.status === 200){
+            var datos = JSON.parse(xmlhttp.responseText);
+            if(resultats.innerHTML === ""){
+                for (var i in datos){
+                    resultats.innerHTML += datos[i].name + "<br/>" + datos[i].birthday + "<br/>" + datos[i].country + "<hr/>";
+                    var a=[],b=[],c=[];
+                    var bahrainNbr=0;
+                    xmlhttp.onload = function (event) {
+                        var datos=JSON.parse(xmlhttp.responseText)
+                        datos.forEach(function(element){
+                        element["score"]=Math.floor(Math.random()*1000);
+                    });
+                    datos.sort(function (a,b) {
+                        return a.score-b.score;
+                    });
+                    console.log(datos);
+                    datos.forEach(function (j) {
+                        if (j.score >750) {
+                            a.push(j);
+                        }
+                        else if (j.score >500) {
+                            b.push(j);
+                        }
+                        else if (j.score <500) {
+                            c.push(j);
+                        }
+                    });
+                    datos.forEach(function (j) {
+                        if (j.country =="Bahrain") {
+                            bahrainNbr++;
+                        }
+                    });
+                    console.log("le plus grand score : ",datos[datos.length-1].score);
+                    };
+                }
+            }
         }
     }
-
     xmlhttp.open("GET", "ex11.json", true);
     xmlhttp.send();
-}   
+}
